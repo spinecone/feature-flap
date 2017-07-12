5c8c2212f578c9533b53eb140ccaac3f4b245e2e
Alejandro Figueroa <alejandrofigueroa@github.com>
2017-07-12 14:20:58 -0400

    Add feature flag helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def feature_flag(feature, &block)
+    feature_flipper = Flipper::Rails.flipper[feature.to_sym]
+
+    feature_flipper.enable_group(:admins) if feature_flipper.state == :off
+
+    return unless feature_flipper.enabled? current_user
+
+    content = capture(&block)
+    content_tag :div, content, class: "feature_flag #{feature}"
+  end
+
+  def feature_enabled?(feature)
+    Flipper::Rails.flipper[feature.to_sym].enabled? current_user
+  end
 end
